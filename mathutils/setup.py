d2fdc3c077c8cb6d9f7398dce63a47bfc28be718
dheeruvaii <dhirajsapkota260@gmail.com>
2024-01-09 15:59:15 +0545

    source distributions file created *sdist

--- mathutils/setup.py
+++ mathutils/setup.py
@@ -1,15 +1,14 @@
-from setuptools import setup,find_packages
-
+from setuptools import setup, find_packages
 
 setup(
     name="mathutils",
-    version="0.10",
-    description="package for math operations and geometry operations",
-    package_data=find_packages(),
+    version="0.1.0",
+    description="A simple Python package for math operations and geometry calculations",
+    packages=find_packages(),
     classifiers=[
-        "Programming Language ::Python ::3",
-        "Liscence :: OSI Approved :: MIT Liscence",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6"
+    python_requires=">=3.6",
 )
